d206a101eee1d0e2bac3e7de37e21a3d88ddb4a2
aagarw30 <test@gmail.com>
2020-02-21 00:02:34 -0500

    added a new file MethodsDemo.java, explanation of how methods work

---
+++ MethodsDemo.java
@@ -0,0 +1,27 @@
+package oopConcepts;
+
+public class MethodsDemo {
+
+	public static void main(String[] args) {
+	String a = "Hey, there !!!";
+	System.out.println(a);		
+	}
+/*
+ * You can have many methods in the java.
+ * However,there should be only ONE main method
+ * Suggestions: Any time you name a method, 
+ * Use camel-casing/Name Convention and also use numerics.
+ * Don't write all code under main method.
+ * Try to write to modified and create more methods.
+ * Additionally, you can not put one method inside another method.
+ * 
+ */
+	
+	public static void method1(String[] args) {
+	String a = "Hey, there !!!";
+     System.out.println(a);
+     
+
+	}
+
+}
