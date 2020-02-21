e073058dd22530d2f57ae0c46e47a7850de6a69e
aagarw30 <test@gmail.com>
2020-02-21 16:33:32 -0500

    Modified AddingMethods.java. It is a return type

--- AddingMethods.java
+++ AddingMethods.java
@@ -3,10 +3,17 @@ package oopConcepts;
 public class AddingMethods {
 
 	public static void main(String[] args) {
-		//Student#1
-		String studentName = "John";
-		
-		int score = 90;
+		findGrades("John", 90);
+		findGrades("Tom", 80);
+		findGrades("Sarah", 95);
+		findGrades("Navid", 85);
+		findGrades("Marouf", 75);
+		findGrades("Usman", 100);
+		findGrades("Khan", 70);
+	}
+	
+	public static void findGrades(String studentName, int score  ) {
+
 		String grade;
 	if (score >= 90 && score <= 100) {
 		grade = "A";
@@ -23,31 +30,14 @@ public class AddingMethods {
 	
 	System.out.println("Grade of  " + studentName + " is: " + grade);
 	
-	
-	
-	// Student#2 
-	
-	String studentName1 = "Steve";
-	
-	int score1 = 80;
-	
-	if (score1 >= 90 && score1 <= 100) {
-	grade = "A";
-	
-	} else if (score1 >= 80 && score1 < 90) {
-	grade = "B";
-	
-	} else if (score1 >= 70 && score1 <80) {
-		grade = "C";
 
-	} else {
-	grade = "D";
+	
+	
+	
 	}
-
-	System.out.println("Grade of  " + studentName1 + " is: " + grade);
-
-	// If there are 10 students and you have to write these code for each student will take a lot of time.
-	// However, there is a method we can use to save all time code.
+	
 	}
-
-}
+	
+		
+	
+	
