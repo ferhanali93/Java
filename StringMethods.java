40cf06c6208f1f3ec1f3cff3b8b488be60b7e51f
aagarw30 <test@gmail.com>
2020-02-20 19:57:28 -0500

    Final Modification of stringMethods.java

--- StringMethods.java
+++ StringMethods.java
@@ -8,6 +8,9 @@ public class StringMethods {
 	String str1 = "Hello";
 	String str2 = "Hello";
 	String str3 = "Welcome";
+	String str4 = "";
+	String str5 = " Spaces all around     ";
+	String str6 = "Hello";
 			
 	//String length: length of the string
 System.out.println(str.length());
@@ -36,33 +39,44 @@ System.out.println(str.endsWith("the")); //False--> because our string does not 
 
 
 //String index: Retruns index of given character value or substring.
-System.out.println(str.indexOf('h'));
+System.out.println(str.indexOf('h')); // It will show 1 in the console because there is only one 'h' in the string.
+System.out.println(str.indexOf('q')); // it will show -1 in the console because there is no 'q' in the string.
 
 
 //String isEmpty: Checks if this string is empty.
-System.out.println(str.isEmpty());
+System.out.println(str.isEmpty()); // It will show false because, the string is not empty. let me create an empty string(str4) to test it.
+System.out.println(str4.isEmpty());// It will show true because, string4 is empty.
 
 
-
-//String replace: Returns a string replacing all the older char to new char
+//String replace: Returns a string replacing all the older char to new char.
+System.out.println(str6.replace('e', 'a')); // we replace a letter 'e' with a letter 'a' in the word "Hello" in string str6.
 
 
 //String subString: Returns a part of the string.
+System.out.println("substring Example 1: " + str.substring(5)); //it will basically print after the 5th index. In this case the word "this" has been replaced.
+System.out.println("Substring Example 2: " + str.substring(5, 10));
+
 
 
 //String toCharArray: Convert this string into character array.
+char[] charArray = str.toCharArray();
+for (int i= 0; i<charArray.length; i++) {
+System.out.println("Index " + i + "is: " + charArray[i]);
+}
+
+//String toLowerCase: Convert the string in the lower-case letter.
+System.out.println("Lower case: " + str.toLowerCase());
 
 
-//String toLowerCase: Returns the sring in the lowercse letter.
+//String toUpperCase: Convert the string in the upper-case letter. 
+System.out.println("Upper Case: " + str.toUpperCase());
 
-
-//String toUpperCase: Returns the strinf in the uppercase letter.
-
-
-//String trim: Eliminating leading and trailing speaces.
+//String trim: Eliminating leading and trailing spaces. basically if you have extra spaces in your string, it will trim it out.
+// example: "   we are humans      "; after using trim, it will remove the spaces around and print it as "we are human" 
+System.out.println(str5.trim());
 	
 	
-	
+
 	}
 
 }
